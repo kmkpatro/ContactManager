0dbb191822a9d4d454a42d9037ed8d4367e2397e
murali <murpat.cloud@gmail.com>
2020-04-26 11:50:42 -0700

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Services;
+
+
+
+namespace ContactManager.Controllers
+{
+    //public class ContactController : ApiController
+    //{
+    //    public Contact[] Get()
+    //    {
+    //        return new Contact[]
+    //        {
+    //    new Contact
+    //    {
+    //        Id = 1,
+    //        Name = "Glenn Block"
+    //    },
+    //    new Contact
+    //    {
+    //        Id = 2,
+    //        Name = "Dan Roth"
+    //    }
+    //        };
+    //    }
+    //}
+
+    //[Route("api/[controller]")]
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        [HttpGet]
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
